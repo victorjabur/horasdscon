6742ffb02bb812037ffd7a0696aed56a5ef4c6bf
Your Name <victorjabur@gmail.com>
2012-02-11 19:02:23 -0200

    ajustes

--- horasdsconapp/google/spreadsheet.py
+++ horasdsconapp/google/spreadsheet.py
@@ -24,17 +24,20 @@ class GoogleSpreadsheet:
         self.clientDocs.http_client.debug = False
         self.clientSpr.auth_token = self.token
         self.clientDocs.auth_token = self.token
+        SCOPES = ['https://spreadsheets.google.com/feeds/', 'https://docs.google.com/feeds/', 'https://www.googleapis.com/auth/userinfo#email']
         oauth_input_params = gdata.auth.OAuthInputParams(gdata.auth.OAuthSignatureMethod.HMAC_SHA1, settings.util.getEntry('google_oauth', 'CONSUMER_KEY'), consumer_secret=settings.util.getEntry('google_oauth', 'CONSUMER_SECRET'))
-        oauth_token = gdata.auth.OAuthToken(key=self.token.token, secret=self.token.token_secret, scopes=settings.GOOGLE_OAUTH_EXTRA_SCOPE, oauth_input_params=oauth_input_params)
+        oauth_token = gdata.auth.OAuthToken(key=self.token.token, secret=self.token.token_secret, scopes=SCOPES, oauth_input_params=oauth_input_params)
         self.clientDB._GetSpreadsheetsClient().SetOAuthInputParameters(gdata.auth.OAuthSignatureMethod.HMAC_SHA1, settings.util.getEntry('google_oauth', 'CONSUMER_KEY'), consumer_secret=settings.util.getEntry('google_oauth', 'CONSUMER_SECRET'))
         self.clientDB._GetDocsClient().SetOAuthInputParameters(gdata.auth.OAuthSignatureMethod.HMAC_SHA1, settings.util.getEntry('google_oauth', 'CONSUMER_KEY'), consumer_secret=settings.util.getEntry('google_oauth', 'CONSUMER_SECRET'))
         self.clientDB._GetDocsClient().SetOAuthToken(oauth_token)
 
     def GetOAuthToken(self, access_token):
-        oauth_token_secret = re.search(r'oauth_token_secret=(.*)&oauth_token=(.*)', access_token).group(1)
-        oauth_token = re.search(r'oauth_token_secret=(.*)&oauth_token=(.*)', access_token).group(2)
-        oauth_token_secret = urllib.unquote(oauth_token_secret)
-        oauth_token = urllib.unquote(oauth_token)
+        oauth_token = '1/DRcGn0-Myv8DXw3tYTINobpwUMfwcRezINeOa6vW-qE'
+        oauth_token_secret = 'aSlyuQYUjVhUFu-DhrF3hmUp'
+        #oauth_token_secret = re.search(r'oauth_token_secret=(.*)&oauth_token=(.*)', access_token).group(1)
+        #oauth_token = re.search(r'oauth_token_secret=(.*)&oauth_token=(.*)', access_token).group(2)
+        #oauth_token_secret = urllib.unquote(oauth_token_secret)
+        #oauth_token = urllib.unquote(oauth_token)
         return gdata.gauth.OAuthHmacToken(
             settings.util.getEntry('google_oauth', 'CONSUMER_KEY'),
             settings.util.getEntry('google_oauth', 'CONSUMER_SECRET'),
@@ -75,7 +78,9 @@ class GoogleSpreadsheet:
         loginInfo = pmo.login(usuario_pmo, senha_pmo)
         colaborador = pmo.extrairColaboradorFromPagina(loginInfo.pagina)
         db = self.clientDB.CreateDatabase(settings.NOME_PLANILHA_HORAS)
-        tabela = db.CreateTable('config', ['usuariopmo','senhapmo','idcolaborador','nomecolaborador'])
+        tabela = db.CreateTable('addresses', ['a','b','c','d'])
+
+
         dados = {'usuariopmo':usuario_pmo, 'senhapmo':senha_pmo, 'idcolaborador':colaborador.id, 'nomecolaborador':colaborador.nome}
         row = tabela.AddRecord(dados)
         tabela = db.GetTables(name='Sheet 1')[0]
