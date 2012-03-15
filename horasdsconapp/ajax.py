e00c81b3d00fd1dfa8bb2e7e87be95cb08202661
Your Name <victorjabur@gmail.com>
2012-03-15 19:02:41 -0300

    ajustes

--- horasdsconapp/ajax.py
+++ horasdsconapp/ajax.py
@@ -4,14 +4,36 @@ from dajaxice.decorators import dajaxice_register
 
 @dajaxice_register
 def updatecomboprojetos(request, option):
-    dajax = Dajax()
-    options = [ ['Madrid','Barcelona','Vitoria','Burgos'],
-        ['Paris','Evreux','Le Havre','Reims'],
-        ['London','Birmingham','Bristol','Cardiff'],]
+    #dajax = Dajax()
+    option = str(option.encode('utf-8'))
+    lista_projetos = request.session['lista_projetos']
     out = ''
-    for o in options[int(option)]:
-        out += "<option value='#'>%s" % (o)
-
+    for projeto in lista_projetos:
+        if projeto.company == option:
+            out += "<option value='%s'>%s" % (projeto.projectname, projeto.projectname)
     #dajax.assign('#combo2','innerHTML',out)
     #return dajax.json()
-    return simplejson.dumps({'message':'Hello World'})
+    return simplejson.dumps({'result':out})
+
+@dajaxice_register
+def dajaxice_example(request):
+    return simplejson.dumps({'message':'Hello from Python!'})
+
+@dajaxice_register
+def example1(request):
+    """ First simple example """
+    return simplejson.dumps({'message': 'hello world'})
+
+@dajaxice_register
+def example2(request):
+    """ Second simple example """
+    return simplejson.dumps({'numbers': [1, 2, 3]})
+
+@dajaxice_register
+def example3(request, data, name):
+    result = sum(map(int, data))
+    return simplejson.dumps({'result': result})
+
+@dajaxice_register
+def error_example(request):
+    raise Exception("Some Exception")
